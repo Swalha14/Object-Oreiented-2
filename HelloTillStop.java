c84bc3346bb23b919cf4f678482cab99357154ea
Swalha <swalhambarak887@gmail.com>
2025-05-05 15:13:07 +0300

    Challenge 5

---
+++ HelloTillStop.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class HelloTillStop {
+    Scanner sc=new Scanner(System.in);
+
+    void showHello(){
+        String choice;
+        System.out.println("For hello type h,anything else to stop");
+        choice= sc.next();
+        while("h".equals(choice)){
+            System.out.println("Hello");
+            choice= sc.next();
+        }
+    }
+    public static void main(String[]args){
+        new HelloTillStop().showHello();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
