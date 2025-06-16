557444976886fc47a84236f5147ae3c99de8b265
Swalha <swalhambarak887@gmail.com>
2025-06-16 14:31:34 +0300

    Array Challenge

---
+++ Challenge7/ArrayChallenge.java
@@ -0,0 +1,29 @@
+package Challenge7;
+
+public class ArrayChallenge {
+
+
+    public static void main(String[] args) {
+        String[]IDs={"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179","B238","A789","C654"};
+
+       for(String item:IDs) {
+
+           if(item.startsWith("B")){
+               System.out.println(item);
+           }
+
+
+       }
+
+
+    }
+
+
+    }
+
+
+
+
+
+
+
---
+++ Challenge7/ArraySortReverse.java
@@ -0,0 +1,36 @@
+package Challenge7;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ArraySortReverse {
+    public static void main (String[]args){
+      String[] pallets ={"B14","A11","B12","A13"};
+
+       //Sort the array in ascending order/alphabetical order
+        System.out.println("Sorted ....");
+
+        Arrays.sort(pallets);//does the sorting of the array
+        for (String pallet : pallets) {
+            System.out.println("-- " + pallet);
+        }
+
+
+        // Reverse the sorted array
+        System.out.println("\nReversed ....");
+        List<String> palletList = Arrays.asList(pallets);
+        //Arrays.asList()- Converts array into List to enable it to be reversed.
+
+        Collections.reverse(palletList); // Reverses the order
+        for (String pallet : palletList) {
+            System.out.println("-- " + pallet);
+        }
+
+
+
+    }
+
+
+
+}
